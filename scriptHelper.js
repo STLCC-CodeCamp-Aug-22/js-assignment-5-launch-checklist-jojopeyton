38ee92af7ac819db309a9807af08aeea022aa9fa
Jordon Peyton <jordon.peyton17@gmail.com>
2022-09-30 09:56:57 -0500

    edited files

--- scriptHelper.js
+++ scriptHelper.js
@@ -52,23 +52,23 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     fuelStatus.innerHTML = 'Fuel level too low for launch';
     cargoStatus.innerHTML = 'Cargo mass low enough for launch';
     launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
-    launchStatus.style.color = red;
+    launchStatus.style.color = 'red';
    } else if (fuelLevel >= 10000 && cargoLevel < 10000) {
     cargoStatus.innerHTML = 'Cargo mass low enough for launch';
     fuelStatus.innerHTML = 'Fuel level high enough for launch';
     launchStatus.innerHTML = 'Shuttle Not Ready For Launch';
-    launchStatus.style.color = red;
+    launchStatus.style.color = 'red';
    } else if (fuelLevel < 10000 && cargoLevel > 10000) {
     cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
     fuelStatus.innerHTML = 'Not enough fuel for journey';
     launchStatus.innerHTML = 'Shuttle is Ready for Launch';
-    launchStatus.style.color = red;
+    launchStatus.style.color = 'red';
    }
     else {
     fuelStatus.innerHTML = 'Fuel level high enough for launch';
     cargoStatus.innerHTML = 'Cargo mass low enough for launch';
     launchStatus.innerHTML = 'Shuttle is Ready for Launch';
-    launchStatus.style.color = green;
+    launchStatus.style.color = 'green';
    }
 }
 }
